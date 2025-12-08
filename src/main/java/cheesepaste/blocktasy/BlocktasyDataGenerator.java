8269ab2061565f101bba4edace1556e35587b295
hsz <hushizhih@gmail.com>
2025-12-08 10:40:47 +0800

    init

---
+++ src/main/java/cheesepaste/blocktasy/BlocktasyDataGenerator.java
@@ -0,0 +1,11 @@
+package cheesepaste.blocktasy;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BlocktasyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
